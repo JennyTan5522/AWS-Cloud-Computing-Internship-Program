18afc6930b1c44db1c6f3a035e1101def9d324f4
JennyTan5522 <85146823+JennyTan5522@users.noreply.github.com>
2023-09-28 04:46:51 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,28 @@
 <?php
-$con = mysqli_connect("tarumt-database.crsnlhosmjue.us-east-1.rds.amazonaws.com", "admin", "TheFourMusketeers666", "tarumt_db");
+define('DB_SERVER', 'tar-database.crsnlhosmjue.us-east-1.rds.amazonaws.com');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'TheFourMusketeers666');
+define('DB_DATABASE', 'tarumt_db');
+
+$con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+
+if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
+
+$database = mysqli_select_db($con, DB_DATABASE);
+
+?>
+
+<?php
+
+define('DB_SERVER', 'tar-database.crsnlhosmjue.us-east-1.rds.amazonaws.com');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'TheFourMusketeers666');
+define('DB_DATABASE', 'tarumt_db');
+
+$con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD,DB_DATABASE);
+
 if (!$con) {
     die("Connection Error");
 }
 ?>
+
