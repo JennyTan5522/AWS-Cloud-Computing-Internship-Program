411094dd262d35fef71ee2bc0f7600aefd959a83
JennyTan5522 <85146823+JennyTan5522@users.noreply.github.com>
2023-09-28 04:45:20 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,6 @@
 <?php
-$con = mysqli_connect("php-database.crsnlhosmjue.us-east-1.rds.amazonaws.com", "admin", "TheFourMusketeers666", "tarumt_db");
+$con = mysqli_connect("tarumt-database.crsnlhosmjue.us-east-1.rds.amazonaws.com", "admin", "TheFourMusketeers666", "tarumt_db");
 if (!$con) {
     die("Connection Error");
-}else{
-    echo "Connected successfully!";
 }
 ?>
